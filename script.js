121d020db974d87e5f653d38afcf23c6278a0b3a
SidraSiddique <bsdsf22a030@pucit.edu.pk>
2024-10-08 07:34:38 -0700

    add new js file

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("hello world")
+// random code
+function displayWelcomeMessage() {
+    const message = "Welcome to Creative Mind - Data Science Project!";
+    console.log(message);
+    document.getElementById("welcome").innerText = message;
+}
+function changeBackgroundColor(color) {
+    document.body.style.backgroundColor = color;
+}
+function toggleSectionVisibility(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section.style.display === "none") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+}
+document.getElementById("toggleButton").addEventListener("click", function() {
+    toggleSectionVisibility("contentSection");
+});
+document.getElementById("colorButton").addEventListener("click", function() {
+    const color = prompt("Enter a color to change the background:");
+    changeBackgroundColor(color);
+});
+window.onload = displayWelcomeMessage;
